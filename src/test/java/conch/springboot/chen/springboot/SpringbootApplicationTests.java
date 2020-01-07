fa7ea81fff7180cb8625b3c979d02358e99039b1
chenconch <892293620@qq.com>
2020-01-07 23:52:24 +0800

    新建项目

---
+++ src/test/java/conch/springboot/chen/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,14 @@
+package conch.springboot.chen.springboot;
+
+import org.junit.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+    @org.junit.Test
+    void contextLoads() {
+    }
+
+}
